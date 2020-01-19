19e7e05ac3ed29e45bfe98e0906ec8f305391d34
Abdelaziz Abdelioua <abdelaziz.abdelioua@airgreets.com>
2020-01-19 14:26:44 +0100

    trying new models

--- Classification_KNN/knn.py
+++ Classification_KNN/knn.py
@@ -24,3 +24,13 @@ y = list(clas) # labels 'predict'
 
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1) # begin the action :D
 
+model = KNeighborsClassifier(n_neighbors=9) # Implementing the model to classify based on the trained data and test data
+
+model.fit(x_train, y_train) # check what fits
+acc = model.score(x_train, y_train) # get the accuracy score
+predicted = model.predict(x_test) # get the predicted values
+names = ["unacc", "acc", "good", "vgood"] # create an array to replace the predicted values to their actual meaning instead of numbers
+print(acc)
+
+for x in range(len(predicted)):
+    print("Predicted: ", names[predicted[x]], "Data: ", x_test[x], "Actual: ", names[y_test[x]])
